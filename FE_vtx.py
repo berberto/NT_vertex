Major updates - vectorized FE matrix assembly [Jake] - plotting with fixed triangulation - cell colors overlapping (weights by softmax of gene expression) - added "verbose" and "source_width" options - setting Olig2+ differentiation rates 4x the others

FE_vtx.py
@@ -6,264 +6,266 @@ Created on Mon Jun  1 10:07:58 2020
 @author: andrewg
 """
 
-import sys
+import time
+
 import numpy as np
 import scipy
+from numba import jit
 from scipy.sparse import coo_matrix
-from cells_extra import cells_setup,add_IKNM_properties, ready_to_divide, cells_evolve
 from FE_transitions import T1, rem_collapsed, divide
-import concurrent.futures
-from multiprocessing import Array ,Process
-import matplotlib.pyplot as plt
-import time
-import ctypes
+from cells_extra import cells_setup, add_IKNM_properties, ready_to_divide, cells_evolve
 
-def M(nodes):
-  """
-  Args:
-    nodes is a list of three coordinates.
-  Return:
-    FE map matrix.
-  """
-  va = [nodes[1][0] - nodes[0][0] , nodes[1][1] - nodes[0][1]] 
-  vb = [nodes[2][0] - nodes[0][0] , nodes[2][1] - nodes[0][1]]
-  M=np.array([va , vb])
-  return M.T    
-  
-  
-def I(i,j,d):
-  """
-  Args:
-    d is the absolute value of the determinant of the map matrix M
-  """
-  if ( i - j ):
-    return (1.0/24)* np.abs(d)
-  else:
-    return (1.0/12)* np.abs(d) 
-  
-def nabPhi(M):
-  """
-  Args:
-    M is the FE matrix map.
-  Returns:
-    Forgotten what this is.
-  """
-  N = np.linalg.inv(M).T
-  #nabP_p = np.matmul(N , np.array([ -1,-1]))
-  #nabP_p = -1*N[:,0]-1*N[:,1]
-  #nabP_q = np.matmul(N , np.array([1,0]))
-  nabP_q = N[:,0]
-  #nabP_r = np.matmul(N , np.array([0,1]))
-  nabP_r = N[:,1]
-  nabP_p = -nabP_q - nabP_r
-  return nabP_p, nabP_q , nabP_r   
- 
-def nabPhi2(M):
-  """
-  Args:
-    M is the FE matrix map.
-  Returns:
-    Forgotten what this is.
-  """
-  N = np.linalg.inv(M).T
-  nabP_p = np.matmul(N , np.array([-1,-1]))
-  nabP_q = np.matmul(N , np.array([1,0]))
-  nabP_r = np.matmul(N , np.array([0,1]))
-  return nabP_p, nabP_q , nabP_r    
-  
-def K(i,j,d,nabPhi,v):
-  """
-  FROM FiniteElement
-  i,j are indices of triangle.  So, i,j belong to {0,1,2}.
-  M is a matrix
-  d is the absolute value of the determinant of M
-  v is a constant (the diffusion coefficient)
-  nabPhi is the output from New_nabPhi.
-  This is a contribution to A[triangle[i],triangle[j]] when updating the 
-  matrix with the part of the integral from triangle.
-  """
-  return (1.0/2)*v*np.inner(nabPhi[i],nabPhi[j])*d        
- 
-def W(i,j,d,nabPhi,nodes, previous_nodes):
-  """
-  Args:
-    i,j in {0,1,2}
-    d is the absolute value of det(M), where M is the matrix map
-    nabPhi is the output from nabPhi.
-    nodes is a list of three node coordinates corresponding to a triangle
-    previous_nodes are the positions of the three nodes at the previous time step.
-    
-  """
-  P0 = (nodes[0]-previous_nodes[0]).T
-  P1 = (nodes[1]-previous_nodes[1]).T
-  P2 = (nodes[2]-previous_nodes[2]).T
-  dummy = P0 + P1 + P2 + (nodes[j]-previous_nodes[j]).T
-  return (1.0 / 24 )*d*np.inner(nabPhi[i].T, dummy)
- 
-def b(i,d,d_old,f,old_alpha,dt): 
-  """
-  Args:
-      i is an index of triangle.  That is, i belongs to {0,1,2}.
-      triangle is a list of three node indices.
-      d is the determinant of M.
-      d_old is the determinant of M from the last time step.
-      f is the source vector of length = len(nodes).
-      f is the vector coefficients of the source expressed in terms of hat functions.
-      old_alpha is the vector of coefficients alphas from the previous time step.
-      dt is the time step
-  """
-  dummy =  I(i,0,d) * f[0] * dt
-  dummy += I(i,1,d) * f[1] * dt
-  dummy += I(i,2,d) * f[2] * dt
-  dummy += I(i,0,d_old) * old_alpha[0]
-  dummy += I(i,1,d_old) * old_alpha[1]
-  dummy += I(i,2,d_old) * old_alpha[2]
-  return dummy
- 
+
+@jit(nopython=True)
+def get_M(nodes):
+    """
+    Args:
+      nodes is a list of three coordinates.
+    Return:
+      FE map matrix.
+    """
+    return (nodes[1:] - nodes[0]).transpose((1, 2, 0))
+
+
+@jit(nopython=True)
+def det2d_stack(X):
+    """
+    Calculates the determinant of the 2x2 matrix for the 2nd and 3rd dims (i.e. with a matrix (n,2,2))
+    """
+    det2d = X[:, 0, 0] * X[:, 1, 1] - X[:, 1, 0] * X[:, 0, 1]
+    return det2d
+
+
+@jit(nopython=True)
+def inv2d_stack(M,det):
+    """
+    Inverse of matrix, row-wise
+
+    """
+    N = ((1 / det) * np.stack((np.row_stack((M[:, 1, 1], -M[:, 1, 0])), np.row_stack((-M[:, 0, 1], M[:, 0, 0]))))).T
+    return N
+
+@jit(nopython=True)
+def get_nabla_Phi(M,d):
+    """
+    Args:
+      M is the FE matrix map.
+    Returns:
+      Forgotten what this is.
+    """
+    N = inv2d_stack(M,d)
+    nabP_q = N[:,0]
+    nabP_r = N[:,1]
+    nabP_p = -nabP_q - nabP_r
+    return np.stack((nabP_p, nabP_q, nabP_r))
+
+@jit(nopython=True)
+def get_I(abs_d):
+    """
+    Args:
+      d is the absolute value of the determinant of the map matrix M
+    """
+    return (np.expand_dims(np.expand_dims(abs_d,1),2)/np.array(((12,24,24),(24,12,24),(24,24,12)))).T
+
+
+
+@jit(nopython=True)
+def get_b(new_I, old_I,f, old_alpha, dt):
+    """
+    Args:
+        i is an index of triangle.  That is, i belongs to {0,1,2}.
+        triangle is a list of three node indices.
+        d is the determinant of M.
+        d_old is the determinant of M from the last time step.
+        f is the source vector of length = len(nodes).
+        f is the vector coefficients of the source expressed in terms of hat functions.
+        old_alpha is the vector of coefficients alphas from the previous time step.
+        dt is the time step
+    """
+    dummy = new_I*f*dt + old_I*old_alpha
+    return dummy[:,0]+dummy[:,1]+dummy[:,2]
+
+@jit(nopython=True)
+def get_K(new_abs_det, nabla_Phi, diff_coef):
+    """
+    FROM FiniteElement
+    i,j are indices of triangle.  So, i,j belong to {0,1,2}.
+    M is a matrix
+    d is the absolute value of the determinant of M
+    v is a constant (the diffusion coefficient)
+    nabPhi is the output from New_nabPhi.
+    This is a contribution to A[triangle[i],triangle[j]] when updating the
+    matrix with the part of the integral from triangle.
+    """
+    K_out = 0.5 * diff_coef * np.expand_dims(np.expand_dims(new_abs_det,1),0)*nabla_Phi*np.expand_dims(nabla_Phi,1)
+    K_out = K_out[:,:,:,0]+K_out[:,:,:,1] # the inner product.
+    return K_out
+    ##note that this is a symmetric matrix, so is storing 2x the amount of data it needs to. Could be optimized further.
+
+
+@jit(nopython=True)
+def get_W(new_abs_det, nabla_Phi, new_nodes, prev_nodes):
+    """
+    Args:
+      i,j in {0,1,2}
+      d is the absolute value of det(M), where M is the matrix map
+      nabPhi is the output from nabPhi.
+      nodes is a list of three node coordinates corresponding to a triangle
+      previous_nodes are the positions of the three nodes at the previous time step.
+
+    """
+
+    P = new_nodes - prev_nodes
+    sum_P = P[0]+P[1]+P[2]
+    dummy = sum_P + P
+    W_out = np.expand_dims(np.expand_dims(new_abs_det, 1), 0) * dummy * np.expand_dims(nabla_Phi, 1)
+    W_out = (W_out[:,:,:,0] + W_out[:,:,:,1])/24
+    return W_out
 
 
 class FE_vtx(object):
-  def __init__(self,cells,centroids,concentration,edges_to_nodes,faces_to_nodes):
-    # default parameters: will be set in the "evolve" method,
-    # and specified triangle by triangle (when they are locally defined)
-    self.parameters = {
-        "diff_coeff": 1.,
-        "degr_rate": 0.,
-        "delta_t": 1.,
-        "prod_rate": 0.
-      }
-    self.cells = cells
-    self.centroids = centroids
-    self.concentration = concentration
-    self.edges_to_nodes = edges_to_nodes
-    self.faces_to_nodes = faces_to_nodes
+    def __init__(self, cells, centroids, concentration, edges_to_nodes, faces_to_nodes):
+        # default parameters: will be set in the "evolve" method,
+        # and specified triangle by triangle (when they are locally defined)
+        self.parameters = {
+            "diff_coeff": 1.,
+            "degr_rate": 0.,
+            "delta_t": 1.,
+            "prod_rate": 0.
+        }
+        self.cells = cells
+        self.centroids = centroids
+        self.concentration = concentration
+        self.edges_to_nodes = edges_to_nodes
+        self.faces_to_nodes = faces_to_nodes
 
-  @property
-  def n_face(self):
-    return self.cells.mesh.n_face
+    @property
+    def concentration_triangles (self):
+        nxt = self.cells.mesh.edges.next
+        f_by_e = self.cells.mesh.face_id_by_edge
+        e_to_n = self.edges_to_nodes
+        f_to_n = self.faces_to_nodes
+        return np.array([
+                self.concentration[e_to_n],         # c_{e,0}
+                self.concentration[e_to_n[nxt]],    # c_{e,1}
+                self.concentration[f_to_n[f_by_e]], # c_{e,2}
+            ]).T
 
-  @property
-  def properties(self):
-    return self.cells.properties
+    @property
+    def n_face(self):
+        return self.cells.mesh.n_face
+
+    @property
+    def properties(self):
+        return self.cells.properties
+
+    def evolve(self, diff_coef, prod_rate, degr_rate, dt, vertex=True, move=True, morphogen=True, diff_rates=None,
+               diff_adhesion=None):
+        """
+        Performs one step of the FE method. Computes the new cells object itself.
+        Uses np.linalg.solve
+        Args:
+          new_cells is the new cells object after movement
+          v is the diffusion coefficient
+          prod_rate is the morphogen production rate.
+          dt is the time step
+        -
+        """
+        m = len(self.concentration)
+        nxt = self.cells.mesh.edges.next
+        f_by_e = self.cells.mesh.face_id_by_edge
+        old_verts = self.cells.mesh.vertices.T
+        old_cents = self.centroids
+        n_e = self.cells.mesh.edges.ids.size
+
+        if move:  # move: use vertex model forces if True, else only expansion
+            new_cells = cells_evolve(self.cells, dt, vertex=vertex, diff_rates=diff_rates, diff_adhesion=diff_adhesion)
+            new_verts = new_cells.mesh.vertices.T
+            new_cents = new_cells.mesh.centres.T
+        else:
+            new_cells = self.cells
+            new_verts = old_verts
+            new_cents = old_cents
+        #perform the FE step if morphogen is True, otherwise set concentration to 0
+        if morphogen:
+            f = self.cells.properties['source'] * prod_rate  # source
+
+            ##define the connectivity from the mesh via node ids
+            node_id_tri = np.array((self.edges_to_nodes, self.edges_to_nodes[nxt], self.faces_to_nodes[f_by_e]))
+            node_id_mat = np.stack((node_id_tri,node_id_tri,node_id_tri))
+            node_id_flat,node_idT_flat = node_id_mat.ravel(), (node_id_mat.transpose(1,0,2)).ravel()
+
+            new_nodes = np.array((new_verts, new_verts[nxt], new_cents[f_by_e]))
+            prev_nodes = np.array((old_verts, old_verts[nxt], old_cents[f_by_e]))
+
+            reduced_f = f[f_by_e]
+            reduced_f = np.stack((reduced_f,reduced_f,reduced_f))
+            old_alpha = self.concentration[node_id_tri]
+            new_M = get_M(new_nodes)
+            old_M = get_M(prev_nodes)
+            new_det = det2d_stack(new_M)
+            new_abs_det = np.abs(new_det)
+            old_det = det2d_stack(old_M)
+            old_abs_det = np.abs(old_det)
+            nabla_Phi = get_nabla_Phi(new_M,new_det)
+            new_I = get_I(new_abs_det)
+            old_I = get_I(old_abs_det)
+            b_tri = get_b(new_I, old_I, reduced_f, old_alpha, dt) #checked
+            K_tri = get_K(new_abs_det, nabla_Phi, diff_coef) #checked
+            W_tri = get_W(new_abs_det, nabla_Phi, new_nodes, prev_nodes)
+            A_tri = (1. + degr_rate*dt)*new_I + dt*K_tri + W_tri
+            b_vec = coo_matrix((b_tri.ravel(),(node_id_tri.ravel(),np.zeros(node_id_tri.size,dtype=np.int64))),shape=(m,1))
+            A_mat = coo_matrix((A_tri.ravel(),(node_idT_flat,node_id_flat)),shape=(m,m))
+            A_mat = A_mat.tocsr() #this performs the summations of repeated entries
+            self.concentration = scipy.sparse.linalg.spsolve(A_mat,b_vec)
+        else:
+            self.concentration = np.zeros(m)
+
+        self.cells = new_cells
+        self.centroids = new_cents
 
 
-  def evolve(self,v,prod_rate,degr_rate,dt,vertex=True,move=True,morphogen=True,diff_rates=None,diff_adhesion=None):
-    """
-    Performs one step of the FE method. Computes the new cells object itself.
-    Uses np.linalg.solve
-    Args:
-      new_cells is the new cells object after movement
-      v is the diffusion coefficient
-      prod_rate is the morphogen production rate.
-      dt is the time step
-    -   
-    """
-    m = len(self.concentration)
-    bv = np.zeros(m,dtype=float) #bv stands for b vector
-    A = np.zeros((m,m),dtype=float)
-    nxt=self.cells.mesh.edges.next
-    f_by_e = self.cells.mesh.face_id_by_edge
-    old_verts = self.cells.mesh.vertices.T
-    old_cents = self.centroids
-    
-    if move: # move: use vertex model forces if True, else only expansion
-      new_cells = cells_evolve(self.cells,dt,vertex=vertex,diff_rates=diff_rates,diff_adhesion=diff_adhesion)
-      new_verts = new_cells.mesh.vertices.T
-      new_cents = new_cells.mesh.centres.T
-
-      # # CHECK THAT CENTROID CALCULATIONS CORRESPOND      
-      # new_cents_ = centroids2(new_cells)
-      # alive = np.where(~new_cells.empty())[0]
-      # gone = np.where(new_cells.empty())[0]
-      # empty = np.where(np.all(np.isnan(new_cents), axis=1))[0]
-      # empty_= np.where(np.all(new_cents_ == -1, axis=1))[0]
-      # print("good centroids correspond?        ", np.allclose( new_cents_[alive], new_cents[alive] ))
-      # print("void centroids correspond?        ", np.all(empty_ == empty))
-      # print("gone cells correspond (in either)?", np.all(empty == gone) and np.all(empty_ == gone))
-      # exit()
-
-      verts_vel = new_cells.mesh.velocities.T
-      cents_vel = (new_cents - old_cents)/dt
-    else:
-      new_cells = self.cells
-      new_verts = old_verts
-      new_cents = old_cents
-      verts_vel = np.zeros(np.shape(old_verts))
-      cents_vel = np.zeros(np.shape(old_cents))
-
-    # perform the FE step if morphogen is True, otherwise set concentration to 0
-    if morphogen:
-      f = self.cells.properties['source']*prod_rate #source
-      count=0
-      rows=[]
-      cols=[]
-      entries=[]
-      for e in self.cells.mesh.edges.ids: #modify if cells.mesh.edges.geometry is cylindrical
-        new_nodes = [new_verts[e] ,new_verts[nxt[e]], new_cents[f_by_e[e]]]
-        prev_nodes = [old_verts[e] ,old_verts[nxt[e]], old_cents[f_by_e[e]]]
-        node_id_tri = np.array([self.edges_to_nodes[e],self.edges_to_nodes[nxt[e]] , self.faces_to_nodes[f_by_e[e]] ],dtype=int)
-        reduced_f = f[f_by_e[e]]*np.ones(3) # [0,0,f[f_by_e[e]]]
-        old_alpha = self.concentration[node_id_tri]
-        new_M = M(new_nodes)
-        old_M = M(prev_nodes)
-        d = np.abs(np.linalg.det(new_M))
-        d_old = np.abs(np.linalg.det(old_M))
-        nabla_Phi = nabPhi(new_M)
-        for i in range(3):
-          bv[node_id_tri[i]] += b(i,d,d_old,reduced_f,old_alpha,dt)
-          for j in range(3):
-            A[node_id_tri[i],node_id_tri[j]] += (1. + degr_rate*dt)*I(i,j,d)+ dt*K(i,j,d,nabla_Phi,v)+ W(i,j,d,nabla_Phi,new_nodes, prev_nodes)
-      self.concentration = scipy.linalg.solve(A,bv)
-    else:
-      self.concentration = np.zeros(m)
-
-    self.cells = new_cells
-    self.centroids = new_cents
+    # this is never used anywhere
+    def transitions(self, division=True):
+        if ready is None:
+            ready = ready_to_divide(self.cells)
+        c_by_e = self.concentration[self.edges_to_nodes]
+        c_by_c = self.concentration[self.faces_to_nodes]
+        self.cells = T1(self.cells)  # perform T1 transitions - "neighbour exchange"
+        self.cells, c_by_e = rem_collapsed(self.cells, c_by_e)  # T2 transitions-"leaving the tissue"
+        if division:
+            self.cells, c_by_e, c_by_c = divide(self.cells, c_by_e, c_by_c, ready)
+        self.centroids = self.cells.mesh.centres.T  # centroids2(self.cells)
+        eTn = self.cells.mesh.edges.ids // 3
+        n = max(eTn)
+        cTn = np.cumsum(~self.cells.empty()) + n
+        con_part = c_by_e[::3]
+        cent_part = c_by_c[~self.cells.empty()]
+        self.concentration = np.hstack([con_part, cent_part])
+        self.edges_to_nodes = self.cells.mesh.edges.ids // 3
+        self.faces_to_nodes = cTn
 
 
-  # this is never used anywhere
-  def transitions(self,division=True):
-    if ready is None:
-      ready = ready_to_divide(self.cells)
-    c_by_e = self.concentration[self.edges_to_nodes]
-    c_by_c = self.concentration[self.faces_to_nodes]
-    self.cells = T1(self.cells) #perform T1 transitions - "neighbour exchange"
-    self.cells,c_by_e = rem_collapsed(self.cells,c_by_e) #T2 transitions-"leaving the tissue"
-    if division:
-      self.cells,c_by_e, c_by_c = divide(self.cells,c_by_e,c_by_c,ready)
-    self.centroids = self.cells.mesh.centres.T # centroids2(self.cells)
-    eTn = self.cells.mesh.edges.ids//3
+def build_FE_vtx(cells, concentration_by_edge=None, concentration_by_face=None):
+    cents = cells.mesh.centres.T  # centroids2(cells)
+    eTn = cells.mesh.edges.ids // 3
     n = max(eTn)
-    cTn=np.cumsum(~self.cells.empty())+n
-    con_part=c_by_e[::3]
-    cent_part = c_by_c[~self.cells.empty()]
-    self.concentration = np.hstack([con_part,cent_part])
-    self.edges_to_nodes = self.cells.mesh.edges.ids//3
-    self.faces_to_nodes = cTn
+    fTn = np.cumsum(~cells.empty()) + n  # we just care about the living faces getting the correct node index
+    if concentration_by_face is None or concentration_by_edge is None:
+        m = fTn[-1] + 1
+        con = np.zeros(m)
+    return FE_vtx(cells, cents, con, eTn, fTn)
 
-    
-    
-def build_FE_vtx(cells,concentration_by_edge=None,concentration_by_face=None):
-  cents = cells.mesh.centres.T # centroids2(cells)
-  eTn = cells.mesh.edges.ids/3
-  n = max(eTn)
-  fTn=np.cumsum(~cells.empty())+n #we just care about the living faces getting the correct node index
-  if concentration_by_face is None or concentration_by_edge is None:
-    m = fTn[-1]+1
+
+def build_FE_vtx_from_scratch(size=None, vm_parameters=None, source_data=None, cluster_data=None, differentiation=True):
+    cells = cells_setup(size, vm_parameters, source_data, cluster_data, differentiation=differentiation)
+    add_IKNM_properties(cells)
+    cents = cells.mesh.centres.T  # centroids2(cells)
+    eTn = cells.mesh.edges.ids // 3
+    n = max(eTn)
+    fTn = np.cumsum(~cells.empty()) + n  # we just care about the living faces getting the correct node index
+    m = fTn[-1] + 1
     con = np.zeros(m)
-  return FE_vtx(cells,cents,con,eTn,fTn)
+    return FE_vtx(cells, cents, con, eTn, fTn)
 
-def build_FE_vtx_from_scratch(size=None, vm_parameters=None,source_data=None,cluster_data=None,differentiation=True):
-  cells = cells_setup(size, vm_parameters,source_data,cluster_data,differentiation=differentiation)
-  add_IKNM_properties(cells)
-  cents = cells.mesh.centres.T # centroids2(cells)
-  eTn = cells.mesh.edges.ids//3
-  n = max(eTn)
-  fTn=np.cumsum(~cells.empty())+n #we just care about the living faces getting the correct node index
-  m=fTn[-1]+1
-  con = np.zeros(m)
-  return FE_vtx(cells,cents,con,eTn,fTn)
-  
-    
-   #returns an FE_vtx object
-  
+    # returns an FE_vtx object

neuraltube.py
@@ -9,16 +9,24 @@ import numpy as np
 from datetime import datetime
 
 from NT_vtx import build_NT_vtx, load_NT_vtx
-from plotting import morphogen_video, cells_state_video, combined_video
-from options import (file_prefix, test_output, restart_file,
+from plotting import combined_video
+from options import (file_prefix, test_output, restart_file, verbose,
                 T_sim, T_init, frame_every, init_only, dt, N_frames,
                 simulate, plotting, from_last,
                 vertex, morphogen, move, division,
                 xsize,ysize, 
-                degr_rate, prod_rate, diff_coef, bind_rate,
+                degr_rate, prod_rate, diff_coef, bind_rate, source_width,
                 Kappa, Gamma, Lambda, diff_adhesion,
                 print_options
                 )
+
+import builtins
+def print(*args, **kwargs):
+    if verbose:
+        return builtins.print(*args, **kwargs)
+    else:
+        pass
+
 
 if __name__ == "__main__":
     
@@ -110,7 +118,7 @@ if __name__ == "__main__":
                 neural_tube.transitions(division=division)
                 
                 # set the source of morphogen to be few cells wide
-                neural_tube.set_source_by_x(width=4)
+                neural_tube.set_source_by_x(width=source_width)
 
             print('')
         else:
@@ -153,17 +161,6 @@ if __name__ == "__main__":
         # allNTinit = sorted([x for x in allfiles if "_NT_init.pkl" in x])
         NT_list = [load_NT_vtx(path+"/"+file) for file in allNT]
         # NTinit_list = [load_NT_vtx(path+"/"+file) for file in allNTinit]
-        nodes_list = [
-                    np.vstack([
-                        nt.FE_vtx.cells.mesh.vertices.T[::3],
-                        nt.FE_vtx.centroids[~nt.FE_vtx.cells.empty()]
-                    ]) for nt in NT_list]
-        concs_list = [nt.FE_vtx.concentration   for nt in NT_list]
-        ponis_list = [nt.GRN.state[:,-4:]   for nt in NT_list]
-        cells_list = [nt.FE_vtx.cells   for nt in NT_list]
-        verts_list = [nt.FE_vtx.cells.mesh.vertices.T[::3] for nt in NT_list]
 
-        # cells_state_video(cells_list, ponis_list, path, path+"/video_cells")
-        # morphogen_video(cells_list, nodes_list, concs_list, path, path+"/video_morphogen")#,zmin=0)#,zmax=10)
-        combined_video(cells_list, nodes_list, concs_list, ponis_list, path, path+"/video_combined")#,zmin=0)#,zmax=10)
+        combined_video(NT_list, filename=path+"/video_combined")#,zmin=0)#,zmax=10)
     

plotting.py
@@ -29,6 +29,7 @@ color = {
     "Nkx": "#02DDD3",   # cyan
     "Irx": "#DDB002"    # gold
 }
+colors_array = np.array([mcol.to_rgb(color[gene]) for gene in ['Pax', 'Olig', 'Nkx', 'Irx']])
 
 def _draw_edges(mesh, ax):
     w = mesh.vertices - mesh.vertices.take(mesh.edges.rotate, 1)  # winding
@@ -114,7 +115,7 @@ def _draw_geometry(geometry, ax=None):
         w, h = geometry.width, geometry.height
         # ax.add_patch(plt.Rectangle((-0.5*w, -0.5*h), w, h, fill=False, linewidth=2.0))
 
-def draw_cells(cells,final_width=None,final_height=None, ax=None, colored=True):
+def draw_cells(cells, xlim=[0,1], ylim=[0,1], ax=None, colored=True):
     """
     Modified version of draw from plotting.
     
@@ -123,18 +124,19 @@ def draw_cells(cells,final_width=None,final_height=None, ax=None, colored=True):
         fig = plt.figure()
         ax = fig.gca()
     facecolors = cells.properties.get('color', None)
-    
-    if final_width is None:
-        final_width= cells.mesh.geometry.width
-    if final_height is None:
-        if hasattr(cells.mesh.geometry,'height'):
-            final_height = cells.mesh.geometry.height
-        else:
-            final_height =max(np.abs( cells.mesh.vertices[1]))
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
     ax.set_xticks([])
     ax.set_yticks([])
-    ax.set_xlim([-0.55*final_width,0.55*final_width])
-    ax.set_ylim([-0.55*final_height,0.55*final_height])
+    
+    # if final_width is None:
+    #     final_width= cells.mesh.geometry.width
+    # if final_height is None:
+    #     if hasattr(cells.mesh.geometry,'height'):
+    #         final_height = cells.mesh.geometry.height
+    #     else:
+    #         final_height =max(np.abs( cells.mesh.vertices[1]))
+
         
     mesh = cells.mesh.recentre()
 
@@ -152,35 +154,23 @@ def draw_cells(cells,final_width=None,final_height=None, ax=None, colored=True):
 
 def set_colour_poni_state(cells,poni_state):
     n_face = cells.mesh.n_face
-    source =cells.properties['source']
-    cells.properties['color']=np.ones((n_face, 3)) #to store RGB number for each face
-    for k in range(n_face):
-        m = np.argmax(poni_state[k])
-        if source[k]==1:
-            cells.properties['color'][k] = np.array([1,1,1]) #source
-        elif m==0:
-            cells.properties['color'][k] = mcol.to_rgb(color['Pax'])# np.array([0,0,1]) #Blue, pax high
-        elif m==1:
-            cells.properties['color'][k] = mcol.to_rgb(color['Olig'])# np.array([1,0,0]) #Red, Olig2 high
-        elif m==2:
-            cells.properties['color'][k] = mcol.to_rgb(color['Nkx'])# np.array([0,0,1]) #Green, NKx22 high
-        elif m==3:
-            cells.properties['color'][k] = mcol.to_rgb(color['Irx'])# np.array([0,1,1]) # ?, Irx high 
+    source = cells.properties['source']
+    
+    # set alpha channel as soft-max
+    # alpha channel for each gene (N_cells, genes)
+    alpha = np.exp(poni_state/.3)
+    alpha /= np.sum(alpha, axis=1)[:,None]
 
-        if cells.properties['leaving'][k] == 1:
-            cells.properties['color'][k] = np.array([0,0,0]) # differentating cells
+    cells.properties['color'] = np.matmul(alpha, colors_array)
+
+    # set particular color for differentiating / source cells
+    cells.properties['color'][cells.properties['leaving']==1] = np.array([0,0,0])
+    cells.properties['color'][cells.properties['source']==1] = np.array([1,1,1])
 
 
-def drawShh(nodes, alpha, z_high, z_low, ax=None, final_width=None, final_height=None, size=None, heatmap=True):
-    l=[]
-    r=[]
-    # if not size:
-    #     d_size=10.0
-    # else:
-    #     d_size = size
-    for i in range(len(nodes)):
-        l.append(nodes[i][0])
-        r.append(nodes[i][1])
+def drawShh(coord_tri, concs_tri, xlim=[0,1], ylim=[0,1], zlim=[0,1], ax=None, heatmap=True):
+
+    z_low, z_high = zlim
     if not ax:
         fig = plt.figure()
         if heatmap:
@@ -189,144 +179,92 @@ def drawShh(nodes, alpha, z_high, z_low, ax=None, final_width=None, final_height
             ax = fig.add_subplot(111, projection='3d')
 
     ax.cla() # clear the axis
-    ax.set_xlim([-0.55*final_width,0.55*final_width])
-    ax.set_ylim([-0.55*final_height,0.55*final_height])
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
     ax.set_xticks([])
     ax.set_yticks([])
-    # ax.set_xlim([-0.55*d_size,0.55*d_size])
-    # ax.set_ylim([-0.55*d_size,0.55*d_size])
     if heatmap:
-        tri = Triangulation(nodes[:,0],nodes[:,1])
-        # mask out elongated triangles at the borders
-        mask = TriAnalyzer(tri).get_flat_tri_mask(.05)
-        tri.set_mask(mask)
-        ax.tricontourf(tri, alpha, levels=np.linspace(0.,z_high, 20), cmap=plt.get_cmap('Greens'))
+        tri = Triangulation(coord_tri[:,:,0].ravel(), coord_tri[:,:,1].ravel(), triangles=np.arange(len(coord_tri)*3).reshape(-1,3))
+        ax.tricontourf(tri, concs_tri.ravel(), levels=np.linspace(0.,z_high, 20), cmap=plt.get_cmap('Greens'))
         # refiner = UniformTriRefiner(tri)
         # tri_refi, z_test_refi = refiner.refine_field(alpha, subdiv=3)
         # ax.tricontourf(tri_refi, z_test_refi, levels=np.linspace(0.,z_high, 20))
     else:
-        ax.set_zlim([z_low -0.1 , z_high])
-        Axes3D.plot_trisurf(ax,l,r,alpha)
-    plt.draw()  
-
-def morphogen_video(cells_history, nodes_array, alpha_array, outputdir, name_file, zmin=None, zmax=None, heatmap=True):
-
-    v_max = np.max((np.max(nodes_array[0]) , np.max(nodes_array[-1])))
-    size = 2*v_max
-    if zmax is None:
-        dummy_max=[]
-        for i in range(len(alpha_array)):
-            dummy_max.append(np.max(alpha_array[i]))
-        z_high = max(dummy_max)
-    else:
-        z_high = zmax
-    if zmin is None:
-        dummy_min=[]
-        for i in range(len(alpha_array)):
-            dummy_min.append(np.min(alpha_array[i]))
-        z_low = min(dummy_min)
-    else:
-        z_low = zmin
-
-    fig = plt.figure()
-    ax = fig.add_subplot(nrows=2)
-    # fig.set_size_inches(6,6)
-    i=0
-    frames=[]
-    final_width = cells_history[-1].mesh.geometry.width
-    if hasattr(cells_history[-1].mesh.geometry,'height'):
-        final_height = cells_history[-1].mesh.geometry.height
-    else:
-        final_height =max(np.abs(cells_history[-1].mesh.vertices[1]))
-    for k in range(len(cells_history)):
-        drawShh(nodes_array[i], alpha_array[i], z_high, z_low, ax, size, heatmap=heatmap)
-        if heatmap:
-            _draw_edges(cells_history[k].mesh, ax)
-        i=i+1
-        frame=outputdir+"/image%03i.png" % i
-        fig.savefig(frame,dpi=500,bbox_inches="tight")
-        frames.append(frame)  
-    os.system("cd ")
-    os.system("ffmpeg -framerate 5/1 -i "+outputdir+"/image%03d.png -c:v libx264 -r 30 -pix_fmt yuv420p "+name_file+".mp4") #for Mac computer  
-
-    # for frame in frames: os.remove(frame)  
-
-def cells_state_video(cells_history, poni_state_history, outputdir, name_file):
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    # fig.set_size_inches(6,6)
-    i=0
-    frames=[]
-    final_width = cells_history[-1].mesh.geometry.width
-    if hasattr(cells_history[-1].mesh.geometry,'height'):
-        final_height = cells_history[-1].mesh.geometry.height
-    else:
-        final_height =max(np.abs(cells_history[-1].mesh.vertices[1]))
-    for k in range(len(cells_history)):
-        set_colour_poni_state(cells_history[k],poni_state_history[k])
-        draw_cells(cells_history[k],final_width,final_height, ax)
-        i=i+1
-        frame=outputdir+"/image%03i.png" % i
-        fig.savefig(frame,dpi=500,bbox_inches="tight")
-        frames.append(frame)
-
-    os.system("cd ")
-    os.system("ffmpeg -framerate 5/1 -i "+outputdir+"/image%03d.png -c:v libx264 -r 30 -pix_fmt yuv420p "+name_file+".mp4") #for Mac computer
-
-    # for frame in frames: os.remove(frame)  
+        raise NotImplementedError("surface plot not implemented")
+        # l=[]
+        # r=[]
+        # for i in range(len(nodes)):
+        #     l.append(nodes[i][0])
+        #     r.append(nodes[i][1])
+        # ax.set_zlim([z_low -0.1 , z_high])
+        # Axes3D.plot_trisurf(ax,l,r,alpha)
+    plt.draw()
 
 
-def combined_video(cells_history, nodes_array, alpha_array, poni_state_history, outputdir, name_file,
-            zmin=None, zmax=None, heatmap=True, ffmpeg=False):
 
-    # setup
+def combined_video(NT_list, filename=None,
+            xlim=None, ylim=None, zlim=None, heatmap=True, ffmpeg=False):
 
-    v_max = np.max((np.max(nodes_array[0]) , np.max(nodes_array[-1])))
-    size = 2*v_max
-    if zmax is None:
-        dummy_max=[]
-        for i in range(len(alpha_array)):
-            dummy_max.append(np.max(alpha_array[i]))
-        z_high = max(dummy_max)
-    else:
-        z_high = zmax
-    if zmin is None:
-        dummy_min=[]
-        for i in range(len(alpha_array)):
-            dummy_min.append(np.min(alpha_array[i]))
-        z_low = min(dummy_min)
-    else:
-        z_low = zmin
+    if filename is None:
+        raise ValueError("Provide name for output video file")
+
+    nodes_list = [
+                np.vstack([
+                    nt.FE_vtx.cells.mesh.vertices.T[::3],
+                    nt.FE_vtx.centroids[~nt.FE_vtx.cells.empty()]
+                ]) for nt in NT_list]
+    concs_list = [nt.FE_vtx.concentration   for nt in NT_list]
+    poni_list = [nt.GRN.state[:,-4:]   for nt in NT_list]
+    cells_list = [nt.FE_vtx.cells   for nt in NT_list]
+    # verts_list = [nt.FE_vtx.cells.mesh.vertices.T[::3] for nt in NT_list]
+    concs_tri_list = [nt.FE_vtx.concentration_triangles   for nt in NT_list]
+    coord_tri_list = [nt.FE_vtx.cells.mesh.recentre().triangles   for nt in NT_list]
+
+
+    # calculate bounds for plotting
+    x_min, y_min, z_min = 3*[np.inf]
+    x_max, y_max, z_max = 3*[np.NINF]
+    for coord_tri, conc_tri in zip(coord_tri_list, concs_tri_list):
+        x_min = min(coord_tri[:,:,0].ravel().min(), x_min)
+        y_min = min(coord_tri[:,:,1].ravel().min(), y_min)
+        z_min = min(conc_tri.ravel().min(), z_min)
+
+        x_max = max(coord_tri[:,:,0].ravel().max(), x_max)
+        y_max = max(coord_tri[:,:,1].ravel().max(), y_max)
+        z_max = max(conc_tri.ravel().max(), z_max)
+
+    if xlim is not None:
+        x_min, x_max = xlim
+    if ylim is not None:
+        y_min, y_max = ylim
+    if zlim is not None:
+        z_min, z_max = zlim
+        
+    ax_lims = {'xlim': [x_min, x_max], 'ylim': [y_min, y_max]}
 
     fig, ax = plt.subplots(nrows=2)
     for a in ax:
         for side in ['top', 'bottom', 'left', 'right']:
             a.spines[side].set_visible(False)
-
-    final_width = cells_history[-1].mesh.geometry.width
-    if hasattr(cells_history[-1].mesh.geometry,'height'):
-        final_height = cells_history[-1].mesh.geometry.height
-    else:
-        final_height =max(np.abs(cells_history[-1].mesh.vertices[1]))
     
     def plot_frame(k):
         plt.cla()
         # top panel
         # 1., 0. to be replaced in general by z_high, z_low
-        drawShh(nodes_array[k], alpha_array[k], 1., 0., ax[0],
-            final_width=final_width,final_height=final_height, heatmap=heatmap)
+        drawShh(coord_tri_list[k], concs_tri_list[k],
+            **ax_lims,
+            zlim=[z_min, z_max], ax=ax[0], heatmap=heatmap)
         if heatmap:
-            draw_cells(cells_history[k], final_width, final_height, ax[0], colored=False)
+            draw_cells(cells_list[k], **ax_lims, ax=ax[0], colored=False)
 
         # bottom panel
-        set_colour_poni_state(cells_history[k],poni_state_history[k])
-        draw_cells(cells_history[k], final_width, final_height, ax[1])
+        set_colour_poni_state(cells_list[k],poni_list[k])
+        draw_cells(cells_list[k], **ax_lims, ax=ax[1])
 
     if ffmpeg:
         i=0
         frames=[]
-        for k in range(len(cells_history)):
+        for k in range(len(cells_list)):
             plot_frame(k)
             plt.show()
             exit()
@@ -335,15 +273,15 @@ def combined_video(cells_history, nodes_array, alpha_array, poni_state_history, 
             fig.savefig(frame,dpi=100,bbox_inches="tight")
             frames.append(frame)  
         os.system("cd ")
-        os.system("ffmpeg -framerate 30 -i "+outputdir+"/image%03d.png -c:v libx264 -r 30 -pix_fmt yuv420p "+name_file+".mp4") #for Mac computer  
+        os.system("ffmpeg -framerate 30 -i "+outputdir+"/image%03d.png -c:v libx264 -r 30 -pix_fmt yuv420p "+filename+".mp4") #for Mac computer  
 
         # for frame in frames: os.remove(frame)  
 
     else:
-        frames=range(len(cells_history))
-        dt = 60000./len(cells_history)
+        frames=range(len(cells_list))
+        dt = 60000./len(cells_list)
         ani = FuncAnimation(fig, plot_frame,
                             interval=dt,
                             frames=frames,
                             blit=False)
-        ani.save(f'{name_file}.mp4')
+        ani.save(f'{filename}.mp4')