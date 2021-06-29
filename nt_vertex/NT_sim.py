367614f11850473dc0846c1ddba129b35184d273
berberto <alberto.pzt@gmail.com>
2021-06-29 15:50:48 +0100

    plotting: added option for log10 scale plot

--- nt_vertex/NT_sim.py
+++ nt_vertex/NT_sim.py
@@ -247,7 +247,7 @@ class NT_simulation (object):
     def video(self, duration=60., files='main', log=False):
         if self.plotting:
             NT_list = self.load(files=files)
-            combined_video(NT_list, filename=self.path+"/video_combined", duration=duration)
+            combined_video(NT_list, filename=self.path+"/video_combined", duration=duration, log=log)
         else:
             print("skip plotting")
 
